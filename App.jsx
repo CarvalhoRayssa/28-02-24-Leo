a289d0f1350422a038a0c2156bb14a4184da85ae
CarvalhoRayssa <112430158+CarvalhoRayssa@users.noreply.github.com>
2024-02-28 09:16:01 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+
+
+function App(){
+    return (
+        <div className="App">
+            <h1>
+                Minha Galeria de Imagens
+            </h1>
+        </div>
+    );
+}
+
+
+export default App;
